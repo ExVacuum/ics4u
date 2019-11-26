f713f901fb593b9d6901dc485480f33af37acafe
Silas Bartha <bartsila533@gmail.com>
2019-11-25 22:44:49 -0500

    Added Comments to MigLayout Program

--- graphics/MigLayoutProgram.java
+++ graphics/MigLayoutProgram.java
@@ -1,3 +1,10 @@
+/**
+ * Silas Bartha, Nov. 25, 2019
+ * MigLayout Practice
+ * Also OMOL Dummy Certificate Generation Tool (Do not abuse pls)
+ * Based on similar Python program, created by Evan Pratten
+ */
+
 package graphics;
 
 import java.awt.event.ActionEvent;
@@ -14,25 +21,15 @@ import java.awt.*;
 
 public class MigLayoutProgram extends JFrame {
 
-    Renderer renderer;
+    //Renderer
+    JPanel renderer;
+
+    //URL Components
     String baseURL = "https://www.labour.gov.on.ca/scripts/mpdf/certificatev16.php?id=";
     String exampleCode = "eyJmaXJzdE5hbWUiOiJpbnNlcnRmaXJzdCIsImxhc3ROYW1lIjoiaW5zZXJ0bGFzdCIsImNvdXJzZSI6Imluc2VydGNvdXJzZSIsImZvcmNlZG93bmxvYWQiOjEsImxhbmd1YWdlIjoiZW4iLCJ1c2VybWFpbCI6Imluc2VydG1haWwifQ==";
     String exampleDecode = new String(Base64.getDecoder().decode(exampleCode));
 
     String finalURL;
-
-    public String buildRaw(String first, String surname, String subject, String email){
-        String code = exampleDecode;
-        code = code.replace("insertfirst", first);
-        code = code.replace("insertlast", surname);
-        code = code.replace("insertcourse", subject);
-        code = code.replace("insertmail", email);
-        return code;
-    }
-
-    public String generateURL(String code){
-        return baseURL + Base64.getEncoder().encodeToString(code.getBytes());
-    }
 
     public static void main(String[] args) {
         new MigLayoutProgram();
@@ -47,43 +44,46 @@ public class MigLayoutProgram extends JFrame {
     }
 
     void init(){
-        setTitle("Definitely Legitimate Certificate Issuing Program");
-        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/res/icon.png")));
-        setSize(500,500);
-        renderer = new Renderer();
-        renderer.setBackground(Color.WHITE);
+
+        //Layout
         MigLayout layout = new MigLayout("wrap 6, align center");
-        renderer.setLayout(layout);
+
+        //Header
         JLabel headerLabel = new JLabel("Definitely Legitimate Certificate Issuing Program");
-        renderer.add(headerLabel, "span, gapbottom 10, align label");
+
+        //First Name
         JLabel firstLabel = new JLabel("First Name: ");
         JTextField firstField = new JTextField(10);
-        renderer.add(firstLabel, "span 1, gapy 20, align right");
-        renderer.add(firstField, "span 2, grow, gapy 20");
+
+        //Last Name
         JLabel surnameLabel = new JLabel("Surname: ");
         JTextField surnameField = new JTextField(10);
-        renderer.add(surnameLabel, "span 1, gap unrelated, gapy 20, align right");
-        renderer.add(surnameField, "span 2, grow, gapy 20");
+
+        //Course Name
         JLabel courseLabel = new JLabel("Course: ");
         JTextField courseField = new JTextField(30);
-        renderer.add(courseLabel, "span 1, gapy 20, align right");
-        renderer.add(courseField, "span 5, grow, gapy 20");
+
+        //Email
         JLabel emailLabel = new JLabel("Email (Optional): ");
         JTextField emailField = new JTextField(30);
-        renderer.add(emailLabel, "span 1, gap unrelated, gapy 20, align right");
-        renderer.add(emailField, "span 5, grow, gapy 20");
+
+        //Generation Button
         JButton generateButton = new JButton("Generate!");
         generateButton.setBackground(Color.WHITE.darker());
         generateButton.setForeground(Color.RED);
-        renderer.add(generateButton, "span 3, gapy 20");
+
+        //Open in Browser Button
         JButton openButton = new JButton("Open in Browser");
         openButton.setBackground(Color.WHITE);
         openButton.setVisible(false);
-        renderer.add(openButton, "span 3, gapy 20, wrap");
+
+        //Certificate Pane
         JEditorPane certPane = new JEditorPane(){
             @Override
             public void paintComponent(Graphics g){
                 Graphics2D g2 = (Graphics2D) g;
+
+                //Make Certificate Small
                 g2.scale(0.25, 0.25);
                 super.paintComponent(g2);
             }
@@ -95,23 +95,50 @@ public class MigLayoutProgram extends JFrame {
         certPane.setMaximumSize(certPane.getSize());
         certPane.setHighlighter(null);
         certPane.setVisible(true);
+
+        //Renderer Panel
+        renderer = new JPanel();
+        renderer.setBackground(Color.WHITE);
+        renderer.setLayout(layout);
+        renderer.add(headerLabel, "span, gapbottom 10, align label");
+        renderer.add(firstLabel, "span 1, gapy 20, align right");
+        renderer.add(firstField, "span 2, grow, gapy 20");
+        renderer.add(surnameLabel, "span 1, gap unrelated, gapy 20, align right");
+        renderer.add(surnameField, "span 2, grow, gapy 20");
+        renderer.add(courseLabel, "span 1, gapy 20, align right");
+        renderer.add(courseField, "span 5, grow, gapy 20");
+        renderer.add(emailLabel, "span 1, gap unrelated, gapy 20, align right");
+        renderer.add(emailField, "span 5, grow, gapy 20");
+        renderer.add(generateButton, "span 3, gapy 20");
+        renderer.add(openButton, "span 3, gapy 20, wrap");
         renderer.add(certPane, "span 4, align center");
-        add(renderer);
+
+        //Generation Button Listener
         generateButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
+
+                //Generate URL Using Specified Data
                 finalURL = generateURL(buildRaw(firstField.getText(), surnameField.getText(), courseField.getText(), emailField.getText()));
                 try {
+
+                    //Add Preview in Certificate Pane
                     certPane.setPage(finalURL);
+
+                    //Show "Open in Browser" button
                     openButton.setVisible(true);
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             }
         });
+
+        //"Open in Browser" Button Listener
         openButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
+
+                //Open Previously Generated URL in Browser
                 if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
                     try {
                         Desktop.getDesktop().browse(new URI(finalURL));
@@ -123,19 +150,46 @@ public class MigLayoutProgram extends JFrame {
                 }
             }
         });
+
+        //Setup Frame
+        setTitle("Definitely Legitimate Certificate Issuing Program");
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/res/icon.png")));
+        setSize(500,500);
+        add(renderer);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
     void step(){
+
+        //Refresh Screen, Used Mainly for Certificate Pane
         renderer.repaint();
     }
 
-    private class Renderer extends JPanel {
+    //Create Plaintext URL Tail, Based on Input Data
+    public String buildRaw(String first, String surname, String subject, String email){
 
-        @Override
-        public void paintComponent(Graphics g){
-            super.paintComponent(g);
-        }
+        //Get decoded template
+        String code = exampleDecode;
+
+        //First Name
+        code = code.replace("insertfirst", first);
+
+        //Last Name
+        code = code.replace("insertlast", surname);
+
+        //Course Name
+        code = code.replace("insertcourse", subject);
+
+        //Email Address
+        code = code.replace("insertmail", email);
+
+        //Return Plaintext Tail
+        return code;
+    }
+
+    //Generate Base64 URL Tail, and Append to Base URL
+    public String generateURL(String code){
+        return baseURL + Base64.getEncoder().encodeToString(code.getBytes());
     }
 }
